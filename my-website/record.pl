c6c7b7be248a368566f920cb62fd604dd126e2b2
charul16yadav <95537438+charul16yadav@users.noreply.github.com>
2023-10-31 02:06:47 +0530

    Add files via upload

---
+++ my-website/record.pl
@@ -0,0 +1,5 @@
+sir
+ i trust the quslified and capable managersof the company that they will choose a right person for this job 
+if i am not selected i would like  a constructive feedback from you so i can improve myself and try again 
+but sir i found while researching on the company that goals and mission matches with my valuesand your purpose
+hence i believe i am the right fit for the job as per my current skill and work profile 
